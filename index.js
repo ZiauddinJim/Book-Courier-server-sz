0db3d9e70351710f46274a7e5c4f9af41af9c702
SheikhMdZiauddin <ziauddinjim@outlook.com>
2025-12-12 14:47:26 +0600

    deploy

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ function generateTrackingId() {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
         const db = client.db("BookCourier")
         const usersCollection = db.collection('users');
         const booksCollection = db.collection('books');
@@ -571,8 +571,8 @@ async function run() {
 
         // Section: MongoDB connection check
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
